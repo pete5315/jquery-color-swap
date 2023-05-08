2703d06b2ba15eede4c88667c7b978e67443a4e8
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-05-08 13:15:58 -0500

    initial commit, structure added

---
+++ script.js
@@ -0,0 +1,24 @@
+$(ready)
+//global variables
+let i=0
+
+function ready() {
+//event listeners
+
+
+}
+
+function addRow() {
+i++;
+
+}
+
+function deleteRow() {
+
+
+}
+
+function styleYellow() {
+
+
+}
