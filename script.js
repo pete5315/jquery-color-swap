c4b8cf9ecd7b67a59dd06e1247eac8a4625618a6
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-05-08 13:28:51 -0500

    Requirements completed

--- script.js
+++ script.js
@@ -4,21 +4,34 @@ let i=0
 
 function ready() {
 //event listeners
-
+$('#generate').on('click', addRow)
+$('div').on('click', '#delete', deleteRow);
+$('div').on('click', '#yellow', styleYellow);
 
 }
 
 function addRow() {
 i++;
+$('#rowContainer').append(`
+<div class='row'>
+    <p>${i}
+    <div class='buttonContainer'>
+        <button id='yellow'>Yellow</button>
+        <button id='delete'>Delete</button>
+    </div>
+</div>
 
+
+
+`)
 }
 
 function deleteRow() {
-
+    $(this).closest('.row').remove();
 
 }
 
 function styleYellow() {
-
+    $(this).parent().parent().addClass('yellow')
 
 }
